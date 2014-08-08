3d1499e6ba9b766b37b12b93829232b78ca2a18b
zzmp <zmp@umich.edu>
2014-08-07 20:13:34 -0700

    (feat) Separation of name and path.

--- dependency.js
+++ dependency.js
@@ -1,17 +1,25 @@
 (function(angular) {
 
   // Initialize an external ng instance for familiar testing
-  var ng            = angular.bootstrap(),
-      $http         = ng.get('$http'),
-      $timeout      = ng.get('$timeout'),
-      $document     = ng.get('$document'),
-      module        = angular.module,
-      modules       = {},
-      dependencies  = [];
+  var ng                = angular.bootstrap(),
+      $http             = ng.get('$http'),
+      $timeout          = ng.get('$timeout'),
+      $document         = ng.get('$document'),
+      $exceptionhandler = ng.get('$exceptionHandler'),
+      module            = angular.module,
+      modules           = {},
+      dependencies      = [];
+
+  dependencies.unresolved = {};
+  dependencies.count = 0;
 
   dependencies.defer = function(dependency, path) {
-    if (!~dependencies.indexOf(dependency)) {
+    if (arguments.length === 1) {
+      // Mark module for later loading
+      this.unresolved[dependency] = true;
+
       // Load module
+    } else if (!~dependencies.indexOf(dependency)) {
       $http.get(path)
         .success(function(module) {
           var script = angular.element('<script>').html(module);
@@ -22,7 +30,10 @@
       ;
 
       this.push(dependency);
-      this.count = (this.count || 0) + 1;
+      this.count += 1;
+
+      // Mark module as loaded
+      delete this.unresolved[dependency];
 
       // Defer bootstraping until all dependencies are loaded
       if (!window.name.match(/^NG_DEFER_BOOTSTRAP!/))
@@ -31,10 +42,18 @@
   };
 
   dependencies.resolve = function() {
-    if (!--this.count)
-      angular.resumeBootstrap(dependencies);
+    if (!--this.count) {
+      for (var module in this.unresolved)
+        $exceptionHandler(
+          Error(module +' has no path'), 'All dependencies must include paths');
+      // Only resume bootstrapping if all modules have loaded
+      if (!module)
+        angular.resumeBootstrap(dependencies);
+    }
   };
 
+  // Expose to angular to allow modularization
+  angular.dependency = dependencies.defer.bind(dependencies);
 
   // Decorate the moduleInstance to give access to dependency loading
   angular.module = function(name) {
